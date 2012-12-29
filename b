1c1a7349593daf29796b69aee95e599785fc3300
Joby Poriyth <pcjoby@gmail.com>
2012-12-29 06:54:55 +0000

    Implementation of a language with LET and PROC

--- b
+++ b
@@ -3,3 +3,5 @@
 
 ocamlbuild -classic-display util.byte
 ocamlbuild -classic-display util.native
+ocamlbuild -classic-display proc.byte
+ocamlbuild -classic-display proc.native
---
+++ proc.ml
@@ -0,0 +1,75 @@
+type variable = string
+
+type expression = 
+  | Cons_exp of int (* OCaml int *)
+  | Diff_exp of (expression * expression)
+  | Zero_exp of expression
+  | If_exp of (expression * expression * expression)
+  | Var_exp of variable
+  | Let_exp of (variable * expression * expression)
+  | Proc_exp of (variable * expression)
+  | Call_exp of (expression * expression)
+
+type program = A_program of expression
+
+type value = 
+  | Int of int
+  | Bool of bool
+  | Proc of (variable * expression)
+
+type env = 
+  | Empty_env
+  | Extend_env of (variable * value * env)
+
+
+let empty_env () = Empty_env
+
+let extend_env variable value env = Extend_env (variable, value, env)
+
+let rec apply_env variable = function 
+  | Empty_env -> failwith ("apply_env: binding not found for " ^ variable)
+  | Extend_env (var', val', _) when var' = variable -> val'
+  | Extend_env (_, _, env) -> apply_env variable env
+
+
+let init_env = (* i = 1; v = 5; x = 10 *)
+  extend_env "i" (Int 1) 
+    (extend_env "v" (Int 5)
+       (extend_env "x" (Int 10) 
+          (empty_env ())))
+
+let minus v1 v2 = 
+  match v1, v2 with
+    | Int i1, Int i2 -> Int (i1 - i2)
+    | _, _ -> raise (Invalid_argument "minus: expected Ints")
+
+let is_zero = function
+  | Int n when n = 0 -> true
+  | _ -> false
+
+let rec value_of_exp exp env = 
+  match exp with
+    | Cons_exp i -> Int i
+    | Var_exp var' -> apply_env var' env
+    | Diff_exp (exp1, exp2) -> let v1 = value_of_exp exp1 env in
+                            let v2 = value_of_exp exp2 env in
+                            minus v1 v2
+    | Zero_exp exp -> Bool (is_zero (value_of_exp exp env))
+    | If_exp (exp1, exp2, exp3) -> 
+        (match (value_of_exp exp1 env) with
+          | Bool true -> value_of_exp exp2 env
+          | Bool false -> value_of_exp exp3 env
+          | _ -> raise (Invalid_argument "value_of_exp: expected a boolean expression"))
+    | Let_exp (var', exp1, exp2) ->
+        let new_env = extend_env var' (value_of_exp exp1 env) env in
+        value_of_exp exp2 new_env
+    | Proc_exp (var', exp) -> Proc (var', exp)
+    | Call_exp (rator, rand) ->
+        (match (value_of_exp rator env) with
+          | Proc (var', exp) -> 
+              let let_exp = Let_exp (var', rand, exp) in
+              value_of_exp let_exp env
+          | _ -> raise (Invalid_argument "value_of_exp: expected a procedure"))
+
+let value_of_program = function
+  | A_program exp -> value_of_exp exp init_env
